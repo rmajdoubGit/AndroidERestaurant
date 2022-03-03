02f808327238ead42526e58ccd173a771fd292ab
ramjdoubGit <ray83600@gmail.com>
2022-03-03 13:15:50 +0100

    Ajout new Activity

--- app/src/main/java/fr/isen/majdoub/androiderestaurant/MenuActivity.kt
+++ app/src/main/java/fr/isen/majdoub/androiderestaurant/MenuActivity.kt
@@ -1,14 +1,11 @@
 package fr.isen.majdoub.androiderestaurant
-import android.widget.*
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-
-class HomeActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val mGreetingTextView : TextView
-        val mButton : Button
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_menu)
     }
 }
