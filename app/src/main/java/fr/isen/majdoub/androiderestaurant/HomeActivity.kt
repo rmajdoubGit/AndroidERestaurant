b8363972117df28002c9a6ee72c662621e1d1521
ramjdoubGit <ray83600@gmail.com>
2022-03-03 14:39:01 +0100

    Ajout second activity

--- app/src/main/java/fr/isen/majdoub/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/majdoub/androiderestaurant/HomeActivity.kt
@@ -1,14 +1,34 @@
 package fr.isen.majdoub.androiderestaurant
-import android.widget.*
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.*
 
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val mGreetingTextView : TextView
-        val mButton : Button
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        val mButtonStarter : Button = findViewById<Button>(R.id.StarterButton)
+        val mButtonDish : Button = findViewById<Button>(R.id.DishButton)
+        val mButtonDessert : Button = findViewById<Button>(R.id.DessertButton)
+        val intent = Intent(this, MenuActivity::class.java)
+        mButtonStarter.setOnClickListener {
+
+            intent.putExtra("config","starter")
+            startActivity(intent)
+        }
+
+        mButtonDish.setOnClickListener {
+
+            intent.putExtra("config","dishes")
+            startActivity(intent)
+        }
+
+        mButtonDessert.setOnClickListener {
+            intent.putExtra("config","dishes")
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/fr/isen/majdoub/androiderestaurant/MenuActivity.kt
+++ app/src/main/java/fr/isen/majdoub/androiderestaurant/MenuActivity.kt
@@ -1,11 +1,25 @@
 package fr.isen.majdoub.androiderestaurant
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
+
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
-    }
-}
+        setContentView(R.layout.activity_configuraiton)
+        val intent = intent
+        if (intent != null) {
+            val textview : TextView = findViewById<Button>(R.id.Configtitle)
+            val str= intent.getStringExtra("config")
+            textview.text = str
+
+
+
+            }
+            }
+        }
+
+
